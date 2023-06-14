7f918df18619b28ec9286a85dbc20522b72f5ed3
shijin-benny <mr.shijinbenny361@gmail.com>
2023-06-15 01:39:49 +0530

    db connection completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     CustomerModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'test',
-    //   entities: [],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'shijin',
+      password: 'shijin123',
+      database: 'mydatabase',
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
